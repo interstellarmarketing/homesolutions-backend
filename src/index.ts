8fc934b3c2dc2e591c3adc38d33dda965e571b02
alorslouis <alorslouis@users.noreply.github.com>
2024-11-28 17:11:33 -0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,24 @@ export default class extends WorkerEntrypoint<Env> {
 	}
 }
 
-export class Testes extends WorkerEntrypoint<Env> {
-	async yalla() {
-		return "px"
+
+export class Zips extends WorkerEntrypoint<Env> {
+
+	async newZipMethods() {
+		return new ZipMethods(this.env)
+	}
+
+}
+
+const kvPrefixFormatter = (zip: string) => `zip:${zip}`
+
+class ZipMethods extends RpcTarget {
+
+	private env: Env;
+
+	constructor(env: Env) {
+		super();
+		this.env = env;
 	}
 
 	async listAllZips(): Promise<UspsZipLookupParser[]> {
@@ -51,52 +66,60 @@ export class Testes extends WorkerEntrypoint<Env> {
 		return parsedMetadata.filter(x => !!x) as UspsZipLookupParser[]
 	}
 
-}
+	async uspsZipCheck(zip: string): Promise<UspsZipLookupParser> {
+		const kvStash = this.env.contracting_estimates
 
-export class Zips extends WorkerEntrypoint<Env> {
+		const kvPrefix = kvPrefixFormatter(zip)
 
-	async newZipMethods() {
-		return new ZipMethods(this.env)
-	}
+		const checkZip = await kvStash.getWithMetadata(kvPrefix)
 
-}
+		if (checkZip.metadata !== null) {
+			console.log("hit kv cache")
+			const parsedMetadata = uspsZipLookupParser.safeParse(checkZip.metadata)
 
-export class ZipMethods extends RpcTarget {
-
-	#env: Env
-
-	constructor(env: Env) {
-		super();
-		this.#env = env;
-	}
-
-
-	async listAllZips(): Promise<UspsZipLookupParser[]> {
-		const kvStash = this.#env.contracting_estimates
-
-		const listAll = await kvStash.list({ prefix: "zip:" })
-
-		const parsedMetadata = listAll.keys.map(x => {
-			const parseMeta = uspsZipLookupParser.safeParse(x.metadata)
-			if (parseMeta.success) {
-				return parseMeta.data
-			} else {
-				return false
+			if (parsedMetadata.success) {
+				return parsedMetadata.data
 			}
-		})
+		}
 
-		return parsedMetadata.filter(x => !!x) as UspsZipLookupParser[]
+		const getZipResult = await fetch("https://tools.usps.com/tools/app/ziplookup/cityByZip", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+				"Accept": "application/json, text/javascript, */*; q=0.01",
+				"Sec-Fetch-Site": "same-origin",
+				"Accept-Language": "en-US,en;q=0.9",
+				"Accept-Encoding": "gzip, deflate, br",
+				"Sec-Fetch-Mode": "cors",
+				"Host": "tools.usps.com",
+				"Origin": "https://tools.usps.com",
+				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15",
+				"Referer": "https://tools.usps.com/zip-code-lookup.htm?citybyzipcode",
+				"Connection": "keep-alive",
+				"Sec-Fetch-Dest": "empty",
+				"X-Requested-With": "XMLHttpRequest",
+			},
+			body: new URLSearchParams({
+				zip
+			}),
+			redirect: "follow"
+		});
+
+		if (!getZipResult.ok) {
+			throw new Error("Failed to get zip from USPS");
+		}
+
+		const parsedResult = uspsZipLookupParser.safeParse(await getZipResult.json())
+
+		if (!parsedResult.success) {
+			throw new Error("Failed: parsing returned json from USPS");
+		}
+
+		const uspsJson = parsedResult.data
+
+		await this.env.contracting_estimates.put(kvPrefix, JSON.stringify(uspsJson), { metadata: uspsJson })
+
+		return uspsJson
 	}
 }
 
-//export class OGWorker extends WorkerEntrypoint<Env> {
-//	async og(f: string, inComponentAsString?: string) {
-//		return new OGImage().image(f, inComponentAsString)
-//	}
-//}
-//
-//export class OGImage extends RpcTarget {
-//	image(f: string, inComponentAsString?: string) {
-//		return OG(f, inComponentAsString)
-//	}
-//}
