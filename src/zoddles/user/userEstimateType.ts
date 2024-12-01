0806d8258417fc820894fe62094ba0463d4cc031
alorslouis <alorslouis@users.noreply.github.com>
2024-11-30 19:43:11 -0800

    zoddle/userEstimate parser (mv from app)

---
+++ src/zoddles/user/userEstimateType.ts
@@ -0,0 +1,19 @@
+import { z } from "zod"
+
+export const estimateParser = z.object({
+	streetAddress: z.string().min(5).trim(),
+	city: z.string().min(2).trim(),
+	state: z.string().length(2).trim(),
+	zipCode: z.string().length(5),
+	firstName: z.string().min(2).trim(),
+	lastName: z.string().min(2).trim(),
+	email: z.string().email().trim(),
+	phone: z.string().regex(/^\(\d{3}\)\s\d{3}-\d{4}$/),
+	isHomeowner: z.boolean(),
+	estimateShortTrade: z.string(),
+	estimateAction: z.string(),
+	estimateType: z.string().nullable()
+})
+
+export type EstimateStoreType = z.infer<typeof estimateParser>
+
