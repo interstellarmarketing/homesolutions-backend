c3c8cd883f272e17a13fbcc162f773b59862643e
alorslouis <alorslouis@users.noreply.github.com>
2024-11-30 19:43:18 -0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,128 @@
-import { RpcTarget, WorkerEntrypoint } from 'cloudflare:workers';
+import { RpcTarget, WorkerEntrypoint, WorkflowEntrypoint, WorkflowStep, WorkflowEvent } from 'cloudflare:workers';
 import { uspsZipLookupParser, UspsZipLookupParser } from './zoddles/usps/uspsLookup';
+import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1';
+import { submissions, users } from './db/schema';
+import { and, eq } from 'drizzle-orm';
+import { EstimateStoreType } from './zoddles/user/userEstimateType';
+import * as schema from "./db/schema";
 
 export default class extends WorkerEntrypoint<Env> {
 	async fetch() {
 		return new Response('Hello from Worker B');
 	}
 
-	async parse() {
-		return "test"
+}
+
+//export class TestWorfklow extends WorkflowEntrypoint<Env> {
+//
+//	async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
+//
+//		const step1 = await step.do("step 1", async () => {
+//			return {
+//				inputParams: event,
+//				files: [
+//					'doc_7392_rev3.pdf',
+//					'report_x29_final.pdf',
+//					'memo_2024_05_12.pdf',
+//					'file_089_update.pdf',
+//					'proj_alpha_v2.pdf',
+//					'data_analysis_q2.pdf',
+//					'notes_meeting_52.pdf',
+//					'summary_fy24_draft.pdf',
+//				],
+//			};
+//		})
+//
+//
+//	}
+//}
+
+class D1 extends RpcTarget {
+
+	env: Env;
+	db: DrizzleD1Database<typeof schema>
+
+	constructor(env: Env) {
+		super();
+		this.env = env;
+		this.db = drizzle(env.DB, { schema })
 	}
 
-	async fuckThisShit(testString: string) {
 
-		return testString.split("").map(x => x + "xx").join("")
+	async listAllRecords(): Promise<schema.SelectUsersWithSubmissions[]> {
+		const getAll = this.db.query.users.findMany({
+			with: {
+				subs: true
+			}
+		})
+
+		return getAll
+	}
+
+	async insertSubmission(userInfo: EstimateStoreType, subSource: schema.SelectSubmissions["source"]): Promise<schema.SelectUsersWithSubmissions> {
+
+		const checkExisting = await this.db.select().from(users).where(and(eq(users.email, userInfo.email), (eq(users.phone, userInfo.phone)))).limit(1)
+
+		const timeNow = new Date(Date.now())
+
+		let returnObject: schema.SelectUsersWithSubmissions
+
+		if (!checkExisting.length) {
+
+			const submitNewUser = await this.db.insert(users).values({
+				...userInfo,
+				createdAt: timeNow
+			}).returning()
+
+			const insertSubmission = await this.db.insert(submissions).values({
+				userId: submitNewUser[0].id,
+				shortTrade: userInfo.estimateShortTrade,
+				action: userInfo.estimateAction,
+				additionalType: userInfo.estimateType,
+				source: subSource,
+				createdAt: timeNow
+			}).returning()
+
+			returnObject = {
+				...submitNewUser[0],
+				subs: insertSubmission
+			}
+		} else {
+			const userId = checkExisting[0]?.id
+
+			const insertSubmission = await this.db.insert(submissions).values({
+				userId: userId,
+				shortTrade: userInfo.estimateShortTrade,
+				action: userInfo.estimateAction,
+				additionalType: userInfo.estimateType,
+				source: subSource,
+				createdAt: timeNow
+			}).returning()
+
+			returnObject = {
+				...checkExisting[0],
+				subs: insertSubmission
+			}
+		}
+
+		return returnObject
+	}
+}
+
+export class D1Worker extends WorkerEntrypoint<Env> {
+	async D1Methods() {
+		return new D1(this.env)
 	}
 }
 
 
 export class Zips extends WorkerEntrypoint<Env> {
-
 	async newZipMethods() {
 		return new ZipMethods(this.env)
 	}
-
 }
 
-const kvPrefixFormatter = (zip: string) => `zip:${zip}`
+const kvPrefixFormatter = (kvPrefix: string, zip: string) => `${kvPrefix}:${zip}`
 
 class ZipMethods extends RpcTarget {
 
@@ -56,7 +153,7 @@ class ZipMethods extends RpcTarget {
 	async uspsZipCheck(zip: string): Promise<UspsZipLookupParser> {
 		const kvStash = this.env.contracting_estimates
 
-		const kvPrefix = kvPrefixFormatter(zip)
+		const kvPrefix = kvPrefixFormatter("zip", zip)
 
 		const checkZip = await kvStash.getWithMetadata(kvPrefix)
 
